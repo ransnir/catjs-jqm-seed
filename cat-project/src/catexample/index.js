5d1febd46acd63f8a2c017c825b2e69688494c21
Ran Snir <ransnir@gmail.com>
2014-05-27 17:01:42 +0300

    seed project

---
+++ cat-project/src/catexample/index.js
@@ -0,0 +1,136 @@
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setUsername", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setUsername";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setUsername
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setEmail", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setEmail";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setEmail
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setPassword", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setPassword";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setPassword
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setRepeatPassword", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setRepeatPassword";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setRepeatPassword
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setmobileweb", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setmobileweb";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setmobileweb
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setweb", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setweb";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setweb
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.slider90", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.slider90";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.slider90
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.clickSignup", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.clickSignup";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.clickSignup
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
---
+++ cat-project/target/catexample/cat/lib/cat.src.js
@@ -0,0 +1,728 @@
+_cat.core.declare('C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setUsername$$cat', {
+  scrap: {
+    "name": ["setUsername"],
+    "embed": ["true"],
+    "jqm": ["setText(\"#username\", \"catjs\");"],
+    "file": "C:/Users/snirr/workspace/catjs_example/cat-project/target/catexample/index.html",
+    "scrapinfo": {
+      "start": {
+        "line": 31,
+        "col": 3
+      },
+      "end": {
+        "line": 35,
+        "col": 5
+      }
+    },
+    "commentinfo": {
+      "start": {
+        "line": 30,
+        "col": 8
+      },
+      "end": {
+        "line": 36,
+        "col": 11
+      }
+    },
+    "single": {
+      "name": true,
+      "embed": true,
+      "jqm": false,
+      "file": true,
+      "scrapinfo": true,
+      "commentinfo": true,
+      "single": true,
+      "singleton": true,
+      "arguments": true,
+      "auto": true,
+      "id": true,
+      "$type": true
+    },
+    "singleton": {
+      "name": -1,
+      "embed": -1,
+      "jqm": -1,
+      "file": -1,
+      "scrapinfo": -1,
+      "commentinfo": -1,
+      "single": -1,
+      "singleton": -1,
+      "arguments": -1,
+      "auto": -1,
+      "id": -1,
+      "$type": -1
+    },
+    "arguments": [],
+    "auto": true,
+    "id": "scrap_2",
+    "$type": "html",
+    "pkgName": "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setUsername"
+  }
+}, 'scrap');
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setUsername$$cat", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setUsername$$cat";
+
+  /* test content in here */
+  _cat.core.ui.setContent({
+    style: 'color:#0080FF',
+    header: 'setUsername',
+    desc: 'setText("#username", "catjs");',
+    tips: ''
+  });
+  _cat.core.plugin('jqm').actions.setText("#username", "catjs");
+});
+_cat.core.declare('C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setEmail$$cat', {
+  scrap: {
+    "name": ["setEmail"],
+    "embed": ["true"],
+    "jqm": ["setText(\"#email\", \"catjsteam@gmail.com\");"],
+    "file": "C:/Users/snirr/workspace/catjs_example/cat-project/target/catexample/index.html",
+    "scrapinfo": {
+      "start": {
+        "line": 44,
+        "col": 12
+      },
+      "end": {
+        "line": 48,
+        "col": 5
+      }
+    },
+    "commentinfo": {
+      "start": {
+        "line": 43,
+        "col": 8
+      },
+      "end": {
+        "line": 49,
+        "col": 5
+      }
+    },
+    "single": {
+      "name": true,
+      "embed": true,
+      "jqm": false,
+      "file": true,
+      "scrapinfo": true,
+      "commentinfo": true,
+      "single": true,
+      "singleton": true,
+      "arguments": true,
+      "auto": true,
+      "id": true,
+      "$type": true
+    },
+    "singleton": {
+      "name": -1,
+      "embed": -1,
+      "jqm": -1,
+      "file": -1,
+      "scrapinfo": -1,
+      "commentinfo": -1,
+      "single": -1,
+      "singleton": -1,
+      "arguments": -1,
+      "auto": -1,
+      "id": -1,
+      "$type": -1
+    },
+    "arguments": [],
+    "auto": true,
+    "id": "scrap_3",
+    "$type": "html",
+    "pkgName": "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setEmail"
+  }
+}, 'scrap');
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setEmail$$cat", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setEmail$$cat";
+
+  /* test content in here */
+  _cat.core.ui.setContent({
+    style: 'color:#0080FF',
+    header: 'setEmail',
+    desc: 'setText("#email", "catjsteam@gmail.com");',
+    tips: ''
+  });
+  _cat.core.plugin('jqm').actions.setText("#email", "catjsteam@gmail.com");
+});
+_cat.core.declare('C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setPassword$$cat', {
+  scrap: {
+    "name": ["setPassword"],
+    "embed": ["true"],
+    "jqm": ["setText(\"#password\", \"123456789\");"],
+    "file": "C:/Users/snirr/workspace/catjs_example/cat-project/target/catexample/index.html",
+    "scrapinfo": {
+      "start": {
+        "line": 56,
+        "col": 12
+      },
+      "end": {
+        "line": 60,
+        "col": 14
+      }
+    },
+    "commentinfo": {
+      "start": {
+        "line": 55,
+        "col": 8
+      },
+      "end": {
+        "line": 61,
+        "col": 11
+      }
+    },
+    "single": {
+      "name": true,
+      "embed": true,
+      "jqm": false,
+      "file": true,
+      "scrapinfo": true,
+      "commentinfo": true,
+      "single": true,
+      "singleton": true,
+      "arguments": true,
+      "auto": true,
+      "id": true,
+      "$type": true
+    },
+    "singleton": {
+      "name": -1,
+      "embed": -1,
+      "jqm": -1,
+      "file": -1,
+      "scrapinfo": -1,
+      "commentinfo": -1,
+      "single": -1,
+      "singleton": -1,
+      "arguments": -1,
+      "auto": -1,
+      "id": -1,
+      "$type": -1
+    },
+    "arguments": [],
+    "auto": true,
+    "id": "scrap_4",
+    "$type": "html",
+    "pkgName": "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setPassword"
+  }
+}, 'scrap');
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setPassword$$cat", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setPassword$$cat";
+
+  /* test content in here */
+  _cat.core.ui.setContent({
+    style: 'color:#0080FF',
+    header: 'setPassword',
+    desc: 'setText("#password", "123456789");',
+    tips: ''
+  });
+  _cat.core.plugin('jqm').actions.setText("#password", "123456789");
+});
+_cat.core.declare('C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setRepeatPassword$$cat', {
+  scrap: {
+    "name": ["setRepeatPassword"],
+    "embed": ["true"],
+    "jqm": ["setText(\"#repeatpassword\", \"123456789\");"],
+    "file": "C:/Users/snirr/workspace/catjs_example/cat-project/target/catexample/index.html",
+    "scrapinfo": {
+      "start": {
+        "line": 68,
+        "col": 3
+      },
+      "end": {
+        "line": 72,
+        "col": 14
+      }
+    },
+    "commentinfo": {
+      "start": {
+        "line": 67,
+        "col": 8
+      },
+      "end": {
+        "line": 73,
+        "col": 11
+      }
+    },
+    "single": {
+      "name": true,
+      "embed": true,
+      "jqm": false,
+      "file": true,
+      "scrapinfo": true,
+      "commentinfo": true,
+      "single": true,
+      "singleton": true,
+      "arguments": true,
+      "auto": true,
+      "id": true,
+      "$type": true
+    },
+    "singleton": {
+      "name": -1,
+      "embed": -1,
+      "jqm": -1,
+      "file": -1,
+      "scrapinfo": -1,
+      "commentinfo": -1,
+      "single": -1,
+      "singleton": -1,
+      "arguments": -1,
+      "auto": -1,
+      "id": -1,
+      "$type": -1
+    },
+    "arguments": [],
+    "auto": true,
+    "id": "scrap_5",
+    "$type": "html",
+    "pkgName": "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setRepeatPassword"
+  }
+}, 'scrap');
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setRepeatPassword$$cat", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setRepeatPassword$$cat";
+
+  /* test content in here */
+  _cat.core.ui.setContent({
+    style: 'color:#0080FF',
+    header: 'setRepeatPassword',
+    desc: 'setText("#repeatpassword", "123456789");',
+    tips: ''
+  });
+  _cat.core.plugin('jqm').actions.setText("#repeatpassword", "123456789");
+});
+_cat.core.declare('C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setmobileweb$$cat', {
+  scrap: {
+    "name": ["setmobileweb"],
+    "embed": ["true"],
+    "jqm": ["setCheck(\"#mobileweb\");"],
+    "file": "C:/Users/snirr/workspace/catjs_example/cat-project/target/catexample/index.html",
+    "scrapinfo": {
+      "start": {
+        "line": 80,
+        "col": 12
+      },
+      "end": {
+        "line": 84,
+        "col": 5
+      }
+    },
+    "commentinfo": {
+      "start": {
+        "line": 79,
+        "col": 2
+      },
+      "end": {
+        "line": 85,
+        "col": 5
+      }
+    },
+    "single": {
+      "name": true,
+      "embed": true,
+      "jqm": false,
+      "file": true,
+      "scrapinfo": true,
+      "commentinfo": true,
+      "single": true,
+      "singleton": true,
+      "arguments": true,
+      "auto": true,
+      "id": true,
+      "$type": true
+    },
+    "singleton": {
+      "name": -1,
+      "embed": -1,
+      "jqm": -1,
+      "file": -1,
+      "scrapinfo": -1,
+      "commentinfo": -1,
+      "single": -1,
+      "singleton": -1,
+      "arguments": -1,
+      "auto": -1,
+      "id": -1,
+      "$type": -1
+    },
+    "arguments": [],
+    "auto": true,
+    "id": "scrap_6",
+    "$type": "html",
+    "pkgName": "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setmobileweb"
+  }
+}, 'scrap');
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setmobileweb$$cat", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setmobileweb$$cat";
+
+  /* test content in here */
+  _cat.core.ui.setContent({
+    style: 'color:#0080FF',
+    header: 'setmobileweb',
+    desc: 'setCheck("#mobileweb");',
+    tips: ''
+  });
+  _cat.core.plugin('jqm').actions.setCheck("#mobileweb");
+});
+_cat.core.declare('C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setweb$$cat', {
+  scrap: {
+    "name": ["setweb"],
+    "embed": ["true"],
+    "jqm": ["setCheck(\"#web\");"],
+    "file": "C:/Users/snirr/workspace/catjs_example/cat-project/target/catexample/index.html",
+    "scrapinfo": {
+      "start": {
+        "line": 88,
+        "col": 12
+      },
+      "end": {
+        "line": 92,
+        "col": 5
+      }
+    },
+    "commentinfo": {
+      "start": {
+        "line": 87,
+        "col": 2
+      },
+      "end": {
+        "line": 93,
+        "col": 5
+      }
+    },
+    "single": {
+      "name": true,
+      "embed": true,
+      "jqm": false,
+      "file": true,
+      "scrapinfo": true,
+      "commentinfo": true,
+      "single": true,
+      "singleton": true,
+      "arguments": true,
+      "auto": true,
+      "id": true,
+      "$type": true
+    },
+    "singleton": {
+      "name": -1,
+      "embed": -1,
+      "jqm": -1,
+      "file": -1,
+      "scrapinfo": -1,
+      "commentinfo": -1,
+      "single": -1,
+      "singleton": -1,
+      "arguments": -1,
+      "auto": -1,
+      "id": -1,
+      "$type": -1
+    },
+    "arguments": [],
+    "auto": true,
+    "id": "scrap_7",
+    "$type": "html",
+    "pkgName": "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setweb"
+  }
+}, 'scrap');
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setweb$$cat", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setweb$$cat";
+
+  /* test content in here */
+  _cat.core.ui.setContent({
+    style: 'color:#0080FF',
+    header: 'setweb',
+    desc: 'setCheck("#web");',
+    tips: ''
+  });
+  _cat.core.plugin('jqm').actions.setCheck("#web");
+});
+_cat.core.declare('C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.slider90$$cat', {
+  scrap: {
+    "name": ["slider90"],
+    "embed": ["true"],
+    "jqm": ["slide(\"#usetesting\" , 90);"],
+    "file": "C:/Users/snirr/workspace/catjs_example/cat-project/target/catexample/index.html",
+    "scrapinfo": {
+      "start": {
+        "line": 111,
+        "col": 3
+      },
+      "end": {
+        "line": 115,
+        "col": 5
+      }
+    },
+    "commentinfo": {
+      "start": {
+        "line": 110,
+        "col": 2
+      },
+      "end": {
+        "line": 116,
+        "col": 5
+      }
+    },
+    "single": {
+      "name": true,
+      "embed": true,
+      "jqm": false,
+      "file": true,
+      "scrapinfo": true,
+      "commentinfo": true,
+      "single": true,
+      "singleton": true,
+      "arguments": true,
+      "auto": true,
+      "id": true,
+      "$type": true
+    },
+    "singleton": {
+      "name": -1,
+      "embed": -1,
+      "jqm": -1,
+      "file": -1,
+      "scrapinfo": -1,
+      "commentinfo": -1,
+      "single": -1,
+      "singleton": -1,
+      "arguments": -1,
+      "auto": -1,
+      "id": -1,
+      "$type": -1
+    },
+    "arguments": [],
+    "auto": true,
+    "id": "scrap_8",
+    "$type": "html",
+    "pkgName": "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.slider90"
+  }
+}, 'scrap');
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.slider90$$cat", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.slider90$$cat";
+
+  /* test content in here */
+  _cat.core.ui.setContent({
+    style: 'color:#0080FF',
+    header: 'slider90',
+    desc: 'slide("#usetesting" , 90);',
+    tips: ''
+  });
+  _cat.core.plugin('jqm').actions.slide("#usetesting", 90);
+});
+_cat.core.declare('C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.clickSignup$$cat', {
+  scrap: {
+    "name": ["clickSignup"],
+    "embed": ["true"],
+    "jqm": ["clickButton(\"#signup\");"],
+    "file": "C:/Users/snirr/workspace/catjs_example/cat-project/target/catexample/index.html",
+    "scrapinfo": {
+      "start": {
+        "line": 123,
+        "col": 3
+      },
+      "end": {
+        "line": 127,
+        "col": 5
+      }
+    },
+    "commentinfo": {
+      "start": {
+        "line": 122,
+        "col": 2
+      },
+      "end": {
+        "line": 128,
+        "col": 5
+      }
+    },
+    "single": {
+      "name": true,
+      "embed": true,
+      "jqm": false,
+      "file": true,
+      "scrapinfo": true,
+      "commentinfo": true,
+      "single": true,
+      "singleton": true,
+      "arguments": true,
+      "auto": true,
+      "id": true,
+      "$type": true
+    },
+    "singleton": {
+      "name": -1,
+      "embed": -1,
+      "jqm": -1,
+      "file": -1,
+      "scrapinfo": -1,
+      "commentinfo": -1,
+      "single": -1,
+      "singleton": -1,
+      "arguments": -1,
+      "auto": -1,
+      "id": -1,
+      "$type": -1
+    },
+    "arguments": [],
+    "auto": true,
+    "id": "scrap_9",
+    "$type": "html",
+    "pkgName": "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.clickSignup"
+  }
+}, 'scrap');
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.clickSignup$$cat", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.clickSignup$$cat";
+
+  /* test content in here */
+  _cat.core.ui.setContent({
+    style: 'color:#0080FF',
+    header: 'clickSignup',
+    desc: 'clickButton("#signup");',
+    tips: ''
+  });
+  _cat.core.plugin('jqm').actions.clickButton("#signup");
+});
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setUsername", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setUsername";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setUsername
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setEmail", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setEmail";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setEmail
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setPassword", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setPassword";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setPassword
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setRepeatPassword", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setRepeatPassword";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setRepeatPassword
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setmobileweb", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setmobileweb";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setmobileweb
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setweb", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setweb";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.setweb
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.slider90", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.slider90";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.slider90
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.clickSignup", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.clickSignup";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.catjs_example.cat-project.target.catexample.index.html.clickSignup
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
